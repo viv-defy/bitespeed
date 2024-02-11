55f6b5b9296dbf32083dd90f902cecfb7d9de407
viv-defy <vishnu@MacBook-Pro.local>
2024-02-11 11:54:38 +0530

    feat: add end point to receive and respond with json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from 'express';
 import { Logger } from './logger';
+import { Handler } from './handler';
 
 const app = express();
-app.use(Logger)
+app.use(express.json());
+app.use(Logger);
 
-app.get('/', (_: Request, res: Response) => {
-    res.send("Hello, World!");
-})
+app.post('/identify', Handler);
 
 app.listen(3000, () => {
     console.log("server listening on port 3000");
-})
+});
